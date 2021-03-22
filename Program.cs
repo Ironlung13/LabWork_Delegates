5e994fab475b4233b45c3fd0c6140603edda5bc8
ironlung13 <ironlung13@mail.ru>
2021-03-22 18:13:19 +0300

    Fix variant

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@ namespace LabWork_Delegates
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Choose task:");
-            Console.WriteLine("1: Func<Action<>, float, float>");
+            Console.WriteLine("1: Func<Action<int>, bool, char, string>");
             Console.WriteLine("2: Ping-Pong");
         Choice:
             switch (Console.ReadLine())
@@ -23,24 +23,43 @@ namespace LabWork_Delegates
                 default:
                     goto Choice;
             }
+
+            Console.WriteLine();
+            Console.WriteLine("To quit, enter \"q\"");
+            Console.WriteLine("To restart program, enter anything else.");
+            switch (Console.ReadLine())
+            {
+                case "q":
+                case "Q":
+                    return;
+                default:
+                    Console.Clear();
+                    Main();
+                    return;
+            }
         }
         public static void TaskNumber1()
         {
-            Func<Action<object>, float, float> func = Task1.Method1;
-            func += Task1.Method2;
-            func += Task1.Method3;
-            Console.WriteLine("Using PrintInfo1 method:");
-            Console.WriteLine(func(Task1.PrintInfo1, 25.0f));
+            Func<Action<int>, bool, char, string> func = Task1Variant6.Method1;
+            func += Task1Variant6.Method2;
+            func += Task1Variant6.Method3;
+
+            Console.WriteLine("Using ActionMethod1 method:");
+            Console.WriteLine(func(Task1Variant6.ActionMethod1, true, 'Y'));
             Console.WriteLine();
-            Console.WriteLine("Using PrintInfo2 method:");
-            Console.WriteLine(func(Task1.PrintInfo2, 39999.0f));
+
+            Console.WriteLine("Using ActionMethod2 method:");
+            Console.WriteLine(func(Task1Variant6.ActionMethod2, false, '!'));
         }
         public static void PlayGame()
         {
             Ping ping = new Ping("Andrew");
             Pong pong = new Pong("Kate");
+            Pong pong1 = new Pong("Mark");
             Game.Play(ping, pong);
-            Console.ReadLine();
+
+            Console.WriteLine();
+            Game.Play(ping, pong1);
         }
     }
 }
---
+++ Task1Variant6.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabWork_Delegates
+{
+    public class Task1Variant6
+    {
+        private static readonly int info1 = 0;
+        private static readonly int info2 = 42069;
+        private static readonly int info3 = -39923;
+        public static string Method1(Action<int> deleg, bool condition, char symbol)
+        {
+            deleg(info1);
+            if (condition)
+            {
+                return new string(symbol, 10);
+            }
+            else
+            {
+                return new string(symbol, 15);
+            }
+        }
+
+        public static string Method2(Action<int> deleg, bool condition, char symbol)
+        {
+            deleg(info2);
+            if (condition)
+            {
+                return new string(symbol, 100);
+            }
+            else
+            {
+                return new string(symbol, 1);
+            }
+        }
+
+        public static string Method3(Action<int> deleg, bool condition, char symbol)
+        {
+            deleg(info3);
+            if (condition)
+            {
+                return new string(symbol, 39);
+            }
+            else
+            {
+                return new string(symbol, 3);
+            }
+        }
+        public static void ActionMethod1(int number)
+        {
+            Console.WriteLine($"Method 1: number is {number}");
+        }
+
+        public static void ActionMethod2(int number)
+        {
+            Console.WriteLine("THIS IS METHOD 2!!!");
+            Console.WriteLine($"Changed number to {number - 10}");
+        }
+    }
+}
